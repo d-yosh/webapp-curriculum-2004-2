fe722ea235e35a4b1f753cf8ca92e541314efe5c
d-yosh <daicedaice87@gmail.com>
2020-01-15 23:03:18 +0900

    for 式に変更

--- src/main/scala/AcademicResults.scala
+++ src/main/scala/AcademicResults.scala
@@ -12,13 +12,11 @@ object AcademicResults extends  App {
   case object ResultNotFound extends Error
 
   def find(name: String): Result = {
-    results.get(name) match {
-      case Some(pointOpt) => pointOpt match {
-        case Some(point) => Point(point)
-        case None => ResultNotFound
-      }
-      case None => StudentNotFound
-    }
+    val result = for {
+      pointOpt <- results.get(name).toRight(StudentNotFound)
+      point <- pointOpt.toRight(ResultNotFound)
+    } yield Point(point)
+    result.merge
   }
 
   println(find("taro")) // Point(90)
